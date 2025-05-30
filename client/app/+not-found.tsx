28766adbedc603e9d617e9b194c3f16d9fb5452d
kunalshah017 <kunalmanishshah@gmail.com>
2025-05-31 02:37:56 +0530

    using RN Text

--- client/app/+not-found.tsx
+++ client/app/+not-found.tsx
@@ -1,13 +1,12 @@
 import { Link, Stack } from 'expo-router';
-import { View } from 'react-native';
-import { Text } from '~/components/nativewindui/Text';
+import { View, Text } from 'react-native'
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View className="flex-1 items-center justify-center bg-background p-5">
-        <Text variant="largeTitle">{"This screen doesn't exist."}</Text>
+        <Text>This screen doesn&apos;t exist.</Text>
 
         <Link href="/" className="m-4 py-4">
           <Text>Go to home screen!</Text>
